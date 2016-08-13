f8744d1082e71fe1671440bad0cf50a6f098fd42
Tim Stephenson <tim@raddonline.com>
2016-08-13 00:02:33 -0700

    Added some home page stuff. Added events and blog.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 
 # Added by Refinery CMS Pages extension
 Refinery::Pages::Engine.load_seed
+
+# Added by Refinery CMS Blog engine
+Refinery::Blog::Engine.load_seed
+
+# Added by Refinery CMS Events extension
+Refinery::Events::Engine.load_seed
